55e36b59d9c891147b3b5a1ed328a7e5c4a2e899
EricX003 <90646214+EricX003@users.noreply.github.com>
2022-12-04 19:50:45 -0600

    Create HollowRhombus.java

---
+++ HollowRhombus.java
@@ -0,0 +1,27 @@
+package CS_Club;
+
+import java.util.Scanner;
+
+public class HollowRhombus {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      
+      // Print a rhombus of side length n
+      // With a 1 star-wide border
+      // Ex, for n = 3
+      //  ***
+      // * *
+      //***
+      
+      // For n = 5
+      //    *****
+      //   *   *
+      //  *   *
+      // *   *
+      //*****
+      
+      
+      in.close();
+   }
+}
